cc39a8a0278ccee1db03bbce5a0bc765c39417b3
Abhishek Maurya <144514324+abhishekmaurya149@users.noreply.github.com>
2023-11-09 20:16:51 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,28 +1,54 @@
+/* ----- Calculator ----- */
 
-//Initialize the variable to store the input expression
-let expression = '';
+function insert(num) {
 
+    var a = document.form.textview;
 
-//     
-
-updateDisplay = () => {
-    document.getElementById('display').value = expression;
+    a.value += num;
 }
-appendToDisplay = (n) => {
-    expression += n;
-    updateDisplay();
-}
-clearDisplay = () => {
-    expression = '';
-    updateDisplay();
-}
-calculate = () => {
-    try{
-        const result = eval(expression);
-        expression = result.toString();
-        updateDisplay();
-    }catch(error){
-        expression = 'error';
-        updateDisplay();
+function opt(amd) {
+    var view = document.form.textview;
+    if (view.value != "") {
+        view.value += amd;
     }
+}
+function zero() {
+    var view = document.form.textview;
+    if (view.value != "") {
+        view.value += '0';
+    }
+}
+function equal() {
+    var button = document.getElementsByClassName('none');
+    var a = document.form.textview;
+    var exp = a.value;
+
+    if (exp) {
+        try {a.value = eval(exp);}
+        catch (e) {
+            alert ("Syntax Error!");
+            document.form.textview.value = 'Syntax Error!';
+            none();
+        }
+    }
+    if (a.value == 0) {
+        a.value = "";
+    }
+    if(a.value == 'Infinity'){
+        document.form.textview.value = "Can't divide by Zero!";
+       // button.onclick ='none()';
+       alert('Can\'t divide by Zero!')
+       none();
+    }
+}
+function none() {
+    alert('Press the Clear button!');
+} 
+function clean() {
+    document.form.textview.value = '';
+}
+
+function back() {
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length-1);
 }
